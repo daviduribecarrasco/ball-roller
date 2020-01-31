b7848bb2e1852dac1b9f72ec19d830019ff169c8
David Uribe <du13@humboldt.edu>
2020-01-30 21:21:05 -0800

    finished ball game tutorial

--- Assets/Script Assets/Playercontroller.cs
+++ Assets/Script Assets/Playercontroller.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text =  "";
+        
     }
 
     void FixedUpdate()
@@ -20,5 +28,27 @@ public class Playercontroller : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if (other.gameObject.CompareTag("Pickup"))
+       {
+           other.gameObject.SetActive(false);
+           count = count + 1;
+           SetCountText ();
+           
+       }
+    }
+
+    void SetCountText ()
+    {
+       countText.text = "Count: " + count.ToString ();
+       
+       if(count >= 12)
+       {
+           winText.text = "You Win!";
+       }
+
     }
 }
